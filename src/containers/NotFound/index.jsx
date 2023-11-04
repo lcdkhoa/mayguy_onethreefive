d0542060bd68403ad25bfe6bc9a786990097c798
lcdkhoa <lcdkhoa@hotmail.com>
2023-11-04 09:53:51 +0700

    update 404 page

--- src/containers/NotFound/index.jsx
+++ src/containers/NotFound/index.jsx
@@ -1,5 +1,20 @@
-function NotFound() {
-	return <h1>404 - Page Not Found under construction</h1>;
-}
+import { Grid, Typography } from '@mui/material';
+
+const NotFound = () => {
+	return (
+		<Grid item container sx={{ height: innerHeight * 0.8 }} xs>
+			<Grid item container justifyContent={'center'} alignContent={'flex-end'}>
+				<Typography variant="h1" color="#1273AA">
+					404
+				</Typography>
+			</Grid>
+			<Grid item container xs justifyContent={'center'}>
+				<Typography variant="body1" color="#1273AA">
+					Looking for something?
+				</Typography>
+			</Grid>
+		</Grid>
+	);
+};
 
 export default NotFound;
