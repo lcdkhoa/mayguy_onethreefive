ef31f5bfed9cc3d6651ff3e1e5d9216203eca5a6
charlie khoa <charlie.khoa@360f.com>
2024-06-06 16:17:40 +0700

    chore: WIP parent form

--- src/containers/ParentForm/index.jsx
+++ src/containers/ParentForm/index.jsx
@@ -30,6 +30,7 @@ const App = () => {
 	});
 
 	const formRef = useRef(null);
+	console.log('formData', formData);
 
 	const handleChange = (e) => {
 		setFormData({
@@ -44,7 +45,8 @@ const App = () => {
 	};
 
 	const generatePDF = async () => {
-		const formElement = formRef.current;
+		// const formElement = formRef.current;
+		const formElement = document.getElementById('main');
 
 		// Capture the form using html2canvas
 		const canvas = await html2canvas(formElement, { scale: 1.5 });
@@ -68,230 +70,203 @@ const App = () => {
 
 	return (
 		<form ref={formRef} onSubmit={handleSubmit}>
-			<Grid
-				container
-				item
-				direction={'column'}
-				id="main"
-				style={{ padding: '20px' }}
-			>
+			<Grid>
 				<Grid
-					id="header"
-					item
 					container
-					xs={12}
-					style={{ marginTop: '-70px', paddingBottom: '20px' }}
-					gutterBottom
+					item
+					direction={'column'}
+					id="main"
+					style={{ padding: '20px' }}
 				>
 					<Grid
+						id="header"
 						item
 						container
 						xs={12}
 						style={{
+							marginTop: '-70px',
+							paddingBottom: '20px',
+							textAlign: 'center',
 							alignItems: 'center',
 							justifyContent: 'center',
 						}}
+						gutterBottom
 					>
-						<img
-							src="/images/parent-form/logo.png"
-							alt="Minh Anh Logo"
-							style={{
-								height: '300px',
-								width: '300px',
-							}}
-						/>
-					</Grid>
-
-					<Grid
-						item
-						container
-						xs={12}
-						direction={'row'}
-						style={{ marginTop: '-60px' }}
-					>
-						<Grid xs={3.5} item></Grid>
-						<Grid xs={1.2} item textAlign={'start'}>
-							<Typography variant="caption1" fontWeight={600}>
-								Hotline/ Zalo:
-							</Typography>
+						<Grid item xs={12}>
+							<img
+								src="/images/parent-form/logo.png"
+								alt="Minh Anh Logo"
+								style={{
+									height: '300px',
+									width: '300px',
+								}}
+							/>
 						</Grid>
-						<Grid xs item>
-							<Typography variant="caption1" fontWeight={600}>
-								094 510 71 10
-							</Typography>
-						</Grid>
-					</Grid>
 
-					<Grid
-						item
-						container
-						xs={12}
-						direction={'row'}
-						style={{ marginTop: '-40px' }}
-					>
-						<Grid xs={3.5} item></Grid>
-						<Grid xs={8} item textAlign={'start'}>
+						<Grid item xs={12} style={{ marginTop: '-80px' }}>
 							<Typography variant="caption1" fontWeight={600}>
+								Hotline/ Zalo: 094 510 71 10 <br />
 								Đường Lê Thanh Nghị, tổ 2, khu 6 - Huyện Côn Đảo, tỉnh Bà Rịa -
 								Vũng Tàu
 							</Typography>
 						</Grid>
-						<Grid xs item>
-							<Typography variant="caption1" fontWeight={600}></Typography>
+					</Grid>
+
+					<Grid item container xs={12} id="title">
+						<Grid item container justifyContent={'center'} xs={12}>
+							<Typography variant="h3" gutterBottom>
+								PHIẾU GHI DANH (Application form)
+							</Typography>
+						</Grid>
+
+						<Grid item container justifyContent={'center'} xs={12}>
+							<Typography variant="subtitle1" gutterBottom>
+								Năm học: 2024 - 2025 (School year)
+							</Typography>
 						</Grid>
 					</Grid>
-				</Grid>
 
-				<Grid item container xs={12} id="part-i">
-					<Grid item container justifyContent={'center'} xs={12}>
-						<Typography variant="h3" gutterBottom>
-							PHIẾU GHI DANH (Application form)
+					<Grid item container xs={12} id="part-i">
+						<Typography variant="h6" gutterBottom>
+							I. Thông tin học viên (Student&apos;s information)
 						</Typography>
-					</Grid>
-					<Grid item container justifyContent={'center'} xs={12}>
-						<Typography variant="subtitle1" gutterBottom>
-							Năm học: 2024 - 2025 (School year)
-						</Typography>
+						<TextField
+							fullWidth
+							label="Tên HV / Full name"
+							margin="normal"
+							name="fullName"
+							value={formData.fullName}
+							onChange={handleChange}
+						/>
+						<TextField
+							fullWidth
+							label="Ngày sinh / DOB"
+							margin="normal"
+							name="dob"
+							value={formData.dob}
+							onChange={handleChange}
+						/>
+
+						<FormControl component="fieldset" margin="normal">
+							<FormLabel component="legend">Giới tính / Sex</FormLabel>
+							<RadioGroup
+								row
+								aria-label="gender"
+								name="gender"
+								value={formData.gender}
+								onChange={handleChange}
+							>
+								<FormControlLabel
+									value="male"
+									control={<Radio />}
+									label="Nam / Male"
+								/>
+								<FormControlLabel
+									value="female"
+									control={<Radio />}
+									label="Nữ / Female"
+								/>
+							</RadioGroup>
+						</FormControl>
+
+						<TextField
+							fullWidth
+							label="Địa chỉ / Address"
+							margin="normal"
+							name="address"
+							value={formData.address}
+							onChange={handleChange}
+						/>
 					</Grid>
 
 					<Typography variant="h6" gutterBottom>
-						I. Thông tin học viên (Student information)
+						II. Thông tin Phụ huynh (Parent&apos;s information)
 					</Typography>
 					<TextField
 						fullWidth
-						label="Tên HV / Full name"
+						label="Tên phụ huynh / Parent's name"
 						margin="normal"
-						name="fullName"
-						value={formData.fullName}
+						name="parentName"
+						value={formData.parentName}
+						onChange={handleChange}
+					/>
+					<Grid container spacing={2}>
+						<Grid item xs={6}>
+							<TextField
+								fullWidth
+								label="Số điện thoại 1 / Phone number 1"
+								margin="normal"
+								name="phone1"
+								value={formData.phone1}
+								onChange={handleChange}
+							/>
+						</Grid>
+						<Grid item xs={6}>
+							<TextField
+								fullWidth
+								label="Số điện thoại 2 / Phone number 2"
+								margin="normal"
+								name="phone2"
+								value={formData.phone2}
+								onChange={handleChange}
+							/>
+						</Grid>
+					</Grid>
+					<TextField
+						fullWidth
+						label="Chữ ký xác nhận / Signature"
+						margin="normal"
+						name="signature"
+						value={formData.signature}
+						onChange={handleChange}
+					/>
+
+					<Typography variant="h6" gutterBottom>
+						III. Thông tin lớp đăng ký (Class information)
+					</Typography>
+					<TextField
+						fullWidth
+						label="Tên lớp / Class name"
+						margin="normal"
+						name="className"
+						value={formData.className}
 						onChange={handleChange}
 					/>
 					<TextField
 						fullWidth
-						label="Ngày sinh / DOB"
+						label="Lịch học (Số buổi / tháng) / Class schedule (Number of classes / month)"
 						margin="normal"
-						name="dob"
-						value={formData.dob}
+						name="classSchedule"
+						value={formData.classSchedule}
 						onChange={handleChange}
 					/>
-
-					<FormControl component="fieldset" margin="normal">
-						<FormLabel component="legend">Giới tính / Sex</FormLabel>
-						<RadioGroup
-							row
-							aria-label="gender"
-							name="gender"
-							value={formData.gender}
-							onChange={handleChange}
-						>
-							<FormControlLabel
-								value="male"
-								control={<Radio />}
-								label="Nam / Male"
-							/>
-							<FormControlLabel
-								value="female"
-								control={<Radio />}
-								label="Nữ / Female"
-							/>
-						</RadioGroup>
-					</FormControl>
-
 					<TextField
 						fullWidth
-						label="Đ/c / Address"
+						label="Học phí / Fee"
 						margin="normal"
-						name="address"
-						value={formData.address}
+						name="fee"
+						value={formData.fee}
+						onChange={handleChange}
+					/>
+					<TextField
+						fullWidth
+						label="Bắt đầu học từ ngày / Start date"
+						margin="normal"
+						name="startDate"
+						value={formData.startDate}
 						onChange={handleChange}
 					/>
 				</Grid>
-
-				<Typography variant="h6" gutterBottom>
-					II. TT PH
-				</Typography>
-				<TextField
-					fullWidth
-					label="Tên PH / Parent's name"
-					margin="normal"
-					name="parentName"
-					value={formData.parentName}
-					onChange={handleChange}
-				/>
-				<Grid container spacing={2}>
-					<Grid item xs={6}>
-						<TextField
-							fullWidth
-							label="SĐT1"
-							margin="normal"
-							name="phone1"
-							value={formData.phone1}
-							onChange={handleChange}
-						/>
-					</Grid>
-					<Grid item xs={6}>
-						<TextField
-							fullWidth
-							label="SĐT2"
-							margin="normal"
-							name="phone2"
-							value={formData.phone2}
-							onChange={handleChange}
-						/>
-					</Grid>
+				<Grid item container justifyContent={'center'} xs={12}>
+					<Button
+						type="submit"
+						variant="contained"
+						color="primary"
+						sx={{ mt: 2 }}
+					>
+						Nộp phiếu
+					</Button>
 				</Grid>
-				<TextField
-					fullWidth
-					label="Chữ ký xác nhận"
-					margin="normal"
-					name="signature"
-					value={formData.signature}
-					onChange={handleChange}
-				/>
-
-				<Typography variant="h6" gutterBottom>
-					II. Thông tin lớp đăng ký
-				</Typography>
-				<TextField
-					fullWidth
-					label="Tên lớp"
-					margin="normal"
-					name="className"
-					value={formData.className}
-					onChange={handleChange}
-				/>
-				<TextField
-					fullWidth
-					label="Lịch học (Số buổi / tháng)"
-					margin="normal"
-					name="classSchedule"
-					value={formData.classSchedule}
-					onChange={handleChange}
-				/>
-				<TextField
-					fullWidth
-					label="HP"
-					margin="normal"
-					name="fee"
-					value={formData.fee}
-					onChange={handleChange}
-				/>
-				<TextField
-					fullWidth
-					label="Bắt đầu học từ ngày"
-					margin="normal"
-					name="startDate"
-					value={formData.startDate}
-					onChange={handleChange}
-				/>
-
-				<Button
-					type="submit"
-					variant="contained"
-					color="primary"
-					sx={{ mt: 2 }}
-				>
-					Submit
-				</Button>
 			</Grid>
 		</form>
 	);
